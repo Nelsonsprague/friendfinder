c8904dbd827fbe9bf4ae1f66b2551102ca5c593a
Nelson <nelsonsprague84@gmail.com>
2019-07-09 16:23:06 -0600

    so jeff can see

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,8 +1,7 @@
-var friendArray = [
-    {
-        "name":"Ahmed",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores":[
+var friendArray = [{
+        "name": "Ahmed",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
             5,
             1,
             4,
@@ -13,9 +12,41 @@ var friendArray = [
             5,
             4,
             1
-          ]
-      }
-      
-];
+        ]
+    }
 
+];
+var userArray = [{
+    "name": "Frank",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores": [
+        3,
+        4,
+        3,
+        2,
+        5,
+        2,
+        2,
+        4,
+        4,
+        5
+    ]
+}]
+
+
+function compare(friendArray, userArray) {
+    const finalarray = [];
+    friendArray.forEach((e1)=> userArray.forEach((e2) => 
+            {
+                if (e1.scores === e2.scores) {
+                    finalarray.push(e1.scores)
+                }
+                console.log(e1.scores);
+                console.log(e2.scores);
+            }
+        ));
+        console.log(finalarray)
+}
+
+compare(friendArray, userArray);
 module.exports = friendArray;
